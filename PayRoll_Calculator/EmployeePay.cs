c99d5e6fa72e13aa0210a2612325b0f90279cf32
132163 <senario.bethel@students.pcci.edu>
2020-01-04 20:40:21 -0600

    Add project files.

---
+++ PayRoll_Calculator/EmployeePay.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace PayRoll_Calculator
+{
+    class EmployeePay
+    {
+        private const double Starting_Pay  = 10.00; /* Minimum employee pay rate           */
+        private const double Maximum_hours = 40.00; /* Maximum hour employee work per week */
+        private string       first_name;            /* First name of the employee          */
+        private string       last_name;             /* Last name of the employee           */
+        private int          id_number;             /* The ID number of the employee       */
+        private double       pay_rate;              /* The pay rate of the employee        */
+
+        double overtime_Hours = 0;
+        double stdPay         = 0;
+        double otPay          = 0;
+
+
+        // Default constructor
+        public EmployeePay()
+        {
+            first_name = null;
+            last_name  = null;
+            id_number  = 0;
+            pay_rate   = Starting_Pay;
+        }
+       
+        // Non-default constructor
+        public EmployeePay(string fName, string lName, int IDnum, double pRate)
+        {
+            first_name = fName;
+            last_name  = lName;
+            id_number  = IDnum;
+            if( pRate >= Starting_Pay)
+                pay_rate = pRate;
+            else
+                throw new ArgumentOutOfRangeException("Pay rate must be $10.00 or more!");
+        }
+
+        // Get and set  the employee first name
+        public string First_Name
+        {
+            get
+            {
+                return first_name;
+            }
+            set
+             {
+                this.first_name = value;
+             }
+        }
+
+        // Get and set the employee last name
+        public string Last_Name
+        {
+            get
+            {
+                return last_name;
+            }
+            set
+            {
+                this.last_name = value;
+            }
+        }
+
+        // Get and set the employee id number
+        public int ID_Number
+        {
+            get
+            {
+                return id_number;
+            }
+            set
+            {
+                if(char.IsDigit((char)id_number))
+                   this.id_number = value;
+                else
+                    throw new FormatException("Invalid data! Please re-enter the information.Numbers only!");
+            }
+        }
+
+        // Get and set the employee pay rate
+        public double Pay_Rate
+        {
+            get
+            {
+                return pay_rate;
+            }
+            set
+            {
+                if (pay_rate >= Starting_Pay)
+                    this.pay_rate = value;
+                else
+                    throw new ArgumentOutOfRangeException("Pay rate must be $10.00 or more!");
+            }
+        }
+
+        // Calculate the employee standard amount
+        public string standard_Pay(double hours_Worked)
+        {
+            if (hours_Worked > Maximum_hours)
+            {
+                overtime_Hours = hours_Worked - Maximum_hours;
+                stdPay         = pay_rate * Maximum_hours;
+            }
+            else if (hours_Worked < 0)
+            {
+                throw new ArgumentOutOfRangeException("Work hours can not be a negative value!");
+            }
+            else
+            {
+                stdPay = pay_rate * hours_Worked;
+            }
+            return stdPay.ToString("c2");
+        }
+
+        // Calculate the employee overtime pay
+        public string Overtime_Pay (double hours_Worked)
+        {
+            otPay = pay_rate * 1.5 * overtime_Hours;
+            return otPay.ToString("c2");
+        }
+
+        // Calculate the gross pay
+       public string Gross_Pay()
+        {
+            double total_Amount = stdPay + otPay;
+            return total_Amount.ToString("c2");
+        }
+    }
+}
---
+++ PayRoll_Calculator/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿#region Heading info
+/**********************************************************************/
+/*                                                                    */
+/* Program Name: program03 - Payroll Calculator                       */
+/* Author:       Senario Bethel                                       */
+/* Installation: Pensacola Christian College, Pensacola, Florida      */
+/* Course:       CS364 .NET Programming                               */
+/* Date Written: February 19, 2019                                    */
+/*                                                                    */
+
+/**********************************************************************/
+/*                                                                    */
+/* This program allows the user to enter in an employee payroll       */
+/* information. A calculation is then performed to display the        */
+/* employee's ID number, full name, standard pay, overtime pay and    */
+/* their gross pay.                                                   */
+/*                                                                    */
+/**********************************************************************/
+#endregion
+
+using System;
+using System.Windows;
+
+namespace PayRoll_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtFirst_name.Focus();
+        }
+
+        private void Calculate_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int    employee_id       = (Int32.Parse (txtID_number.Text));
+                double pay_rate          = (double.Parse(txtPay_rate.Text));
+                double hours_work        = (double.Parse(txtHours_worked.Text));
+
+                EmployeePay employee_info = new EmployeePay(txtFirst_name.Text, txtLast_name.Text, employee_id, pay_rate);
+                Employee_ID.Text          = employee_id.ToString();
+                Fullname.Text             = employee_info.First_Name + " " + employee_info.Last_Name;
+                Standard_Amount.Text      = employee_info.standard_Pay(hours_work);
+                Overtime_Amount.Text      = employee_info.Overtime_Pay(hours_work);
+                Total_Amount.Text         = employee_info.Gross_Pay();
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Error_Message.Content    = ex.ParamName;
+                Error_Message.Visibility = Visibility.Visible;
+            }
+            catch (FormatException fex)
+            {
+                if (txtFirst_name.Text == string.Empty && txtLast_name.Text == string.Empty &&
+                   txtID_number.Text == string.Empty && txtPay_rate.Text == string.Empty &&
+                   txtHours_worked.Text == string.Empty)
+                {
+                    Error_Message.Content = fex.Message;
+                    Error_Message.Content = "Please enter in the employee's information!";
+                }
+                else
+                {
+                    Error_Message.Content = "Please re-enter the ID number. Numbers only!";
+                    Error_Message.Visibility = Visibility.Visible;
+                }
+            }
+            catch(Exception Eex)
+            {
+                Error_Message.Content    = Eex.Message;
+                Error_Message.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Clear_Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear each text box
+            this.txtFirst_name.Text   = string.Empty;
+            this.txtLast_name.Text    = string.Empty;
+            this.txtID_number.Text    = string.Empty;
+            this.txtPay_rate.Text     = string.Empty;
+            this.txtHours_worked.Text = string.Empty;
+            this.Employee_ID.Text     = string.Empty;
+            this.Fullname.Text        = string.Empty;
+            this.Standard_Amount.Text = string.Empty;
+            this.Overtime_Amount.Text = string.Empty;
+            this.Total_Amount.Text    = string.Empty;
+
+            // Place the cursor back into first name text box
+            txtFirst_name.Focus();
+            Error_Message.Visibility = Visibility.Hidden;
+        }
+    }
+}
